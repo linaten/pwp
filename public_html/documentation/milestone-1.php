e29c4dee5dfef99119d5d4f3507665dea1bb2f00
Lindsey Atencio <lindsey.atencio@gmail.com>
2019-10-17 15:38:40 -0600

    commit 1

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE=HTML>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Purpose</title>
+</head>
+<body>
+<h>Purpose</h>
+<p></p>
+<h>Audience</h>
+<p>This site will be used by small businesses in foreign countries who are wanting to add a page to their website in english to attract a wider tourist clientele</p>
+<h>Goal</h>
+<p> The goal of this website is to attract potential freelance clients</p>
+<h1>Persona</h1>
+
+</body>
+</html>
