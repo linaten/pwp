1c063cf67e905f8f0eebdbde2735e7e77e00bf7a
Lindsey Atencio <lindsey.atencio@gmail.com>
2019-10-17 16:12:48 -0600

    commit 2

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,17 +1,26 @@
-<!DOCTYPE=HTML>
+<!DOCTYPE=html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>Purpose</title>
 </head>
 <body>
-<h>Purpose</h>
+<h1>Purpose</h1>
 <p></p>
-<h>Audience</h>
+<h1>Audience</h1>
 <p>This site will be used by small businesses in foreign countries who are wanting to add a page to their website in english to attract a wider tourist clientele</p>
-<h>Goal</h>
+<h1>Goal</h1>
 <p> The goal of this website is to attract potential freelance clients</p>
 <h1>Persona</h1>
-
+<h2>Name</h2>
+<p>Katarina</p>
+<h2>Demographics</h2>
+<p>Katarina is a small business owner who lives in Siberia. Katarina is in her late thirties and has moderate experience with using a computer. Katarina understands that tourism is growing in her town, and has </p>
+<h2>User Goal</h2>
+<p>Katarina's goal for this website is to find a person that can create a website page in English for her company.</p>
+<h2>Frustrations</h2>
+<p>Katarina does speak some English, but does not know how to read or write it.</p>
+<h2>Additional Information</h2>
+<p>A translator or at a minumum google translate will need to be used to translate the website content into Russian</p>
 </body>
 </html>
