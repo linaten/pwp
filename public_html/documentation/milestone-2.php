4acee4ff4f78033f2925dc3c2e025c997eff08cc
Lindsey Atencio <lindsey.atencio@gmail.com>
2019-10-31 07:20:09 -0600

    milestone 2 documentation

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -5,12 +5,18 @@
 	<title>Content Strategy</title>
 	<h1>Content Strategy</h1>
 </head>
+<body>
 	<h2>Welcome</h2>
-	<p>In this section, I will welcome the user to the page. I will then describe the purpose of the site, which is to advertise that I can build a website or webpage for their business in English to attract English-speaking tourists/customers. This section will be writted first in Russian, and then in English. </p>
+	<p>In this section, I will welcome the user to the page. I will then describe the purpose of the site, which is to advertise that I can build a we=[;/bsite or webpage for their business in English to attract English-speaking tourists/customers. This section will be writted first in Russian, and then in English. </p>
 <h2>Examples</h2>
 <p>In this section, I will use two jpeg images to display two example pages of a website. The first will be displaying a restaurant'smenu and information in Russian. The second will be an identical image but with all the text in English. </p>
 <h2>About Me</h2>
 <p>This section will a photo of me and a brief description of who I am. This description will include both information regarding my services as a web developer, my experience as a tourist and a traveler in Russia, and how the combination of both gives me the perfect insight into designing a website or webpage that will attract fellow English-speaking tourists. This section will first be in Russian and then again in English.</p>
 <h2>Contact Information</h2>
 <p>This section will include my contact information. I will display my email address, phone number, etc. These will be described in both languages.</p>
-<body>
+<img src="phone.png">
+	<p>
+		<img src="full.png">
+	</p>
+
+</body>
