9bcebd0a7d12d7f89e5ab4c50d1949e4dbe3c8a6
Lindsey Atencio <lindsey.atencio@gmail.com>
2019-11-11 07:07:56 -0700

    picture of me added

--- public_html/PWP/index.php
+++ public_html/PWP/index.php
@@ -8,8 +8,9 @@
 <h1>Welcome!</h1>
 <p>Are you interested in attracting more English-speaking tourists to your business? I can assist. I build websites. I can create a website for you in English. If you already have a website, I can add an English page to your website.</p>
 <h2>Examples</h2>
-<img src="rmenu.png"
+<img src="rmenu.png">
 <p></p>
 <h2>About Me</h2>
+<img src="facebook.jpg">
 <p>I am a traveler and a freelance writer and computer programming. I have travelled through over 30 countries, and am passionate about experiencing new cultures and locations. As a tourist and a backpacker, I am uniquely qualified to understand what this demographic needs in order to  </p>
 </body>
