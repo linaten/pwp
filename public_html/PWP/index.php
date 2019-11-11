c636d99582706c0ca9b5c74a22146b1c18320665
Lindsey Atencio <lindsey.atencio@gmail.com>
2019-11-11 07:03:18 -0700

    first example menu

---
+++ public_html/PWP/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE=html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Lindsey Atencio</title>
+</head>
+<body>
+<h1>Welcome!</h1>
+<p>Are you interested in attracting more English-speaking tourists to your business? I can assist. I build websites. I can create a website for you in English. If you already have a website, I can add an English page to your website.</p>
+<h2>Examples</h2>
+<img src="rmenu.png"
+<p></p>
+<h2>About Me</h2>
+<p>I am a traveler and a freelance writer and computer programming. I have travelled through over 30 countries, and am passionate about experiencing new cultures and locations. As a tourist and a backpacker, I am uniquely qualified to understand what this demographic needs in order to  </p>
+</body>
